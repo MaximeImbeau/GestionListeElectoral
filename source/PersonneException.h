1e955d0b58910d7f8c934797e2e0f38e5e5d10b9
Nicholas Rompré St-Yves <nicholas.rompre-st-yves.1@ulaval.ca>
2020-12-05 23:40:33 -0500

    J'ai ajouté PersonneException, PersonneDejaPresentException, PersonneAbsenteException.

---
+++ source/PersonneException.h
@@ -0,0 +1,31 @@
+/*
+ * EmployeException.h
+ *
+ *  Created on: 2020-12-05
+ *      Author: etudiant
+ */
+
+#ifndef PERSONNEEXCEPTION_H_
+#define PERSONNEEXCEPTION_H_
+
+#include <stdexcept>
+
+class PersonneException : public std::runtime_error{
+public:
+	PersonneException(const std::string& p_raison):
+		std::runtime_error(p_raison){}
+};
+
+class PersonneDejaPresentException : public PersonneException{
+public:
+	PersonneDejaPresentException(const std::string& p_raison):
+		PersonneException(p_raison){}
+};
+
+class PersonneAbsenteException : public PersonneException{
+public:
+	PersonneAbsenteException(const std::string& p_raison):
+		PersonneException(p_raison){}
+};
+
+#endif /* PERSONNEEXCEPTION_H_ */
